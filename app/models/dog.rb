2759744bb7bf9815c3086d3fef6d2df2c5bd72e2
WilfriedPaillot <paillotwilfried@gmail.com>
2022-02-10 23:52:35 +0100

    Updated models

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,4 @@
 class Dog < ApplicationRecord
+  has_many :strolls, dependent: :destroy
+  has_many :dog_sitters, through: :strolls
 end
--- app/models/dog_sitter.rb
+++ app/models/dog_sitter.rb
@@ -1,2 +1,5 @@
 class DogSitter < ApplicationRecord
+  has_many :strolls, dependent: :destroy
+  has_many :dogs, through: :strolls
+  belong_to :city
 end
